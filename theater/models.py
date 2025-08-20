2e1f83d80894a8a5a8089b17abf4b134314733c7
RomanSapunGit <sapunroma23@gmail.com>
2025-08-20 19:52:12 +0200

    feat(models): add model classes
    Added necessary classes for program to work. Those are: User, Genre, Author, Play, TheatreHall, Ticket, Reservation, Performance.

--- theater/models.py
+++ theater/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name="plays")
+    actors = models.ManyToManyField(Actor, related_name="plays")
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats = models.PositiveIntegerField()
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    theatre_hall = models.ForeignKey(
+        TheatreHall,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
