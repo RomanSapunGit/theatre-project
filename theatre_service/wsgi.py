ae81deb5c7477a9c5ad3fe19d777adc74f1d9fdd
RomanSapunGit <sapunroma23@gmail.com>
2025-08-20 19:53:52 +0200

    refactor(theatre directory): change directory name and update settings.py
    Add apps, auth user model in settings.py.

--- theatre_service/wsgi.py
+++ theatre_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for theatre-service project.
+WSGI config for theatre_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theatre-service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theatre_service.settings")
 
 application = get_wsgi_application()
