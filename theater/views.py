4ff91a35345087a7328646b8d5a0027482afd423
RomanSapunGit <sapunroma23@gmail.com>
2025-08-20 20:52:15 +0200

    feat(theatre views): add basic views
    Added viewsets which inherits from ModelViewSet (ActorViewset from GenericViewSet), custom get serializer classes based on actions. Views will be updated in future commits.

--- theater/views.py
+++ theater/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+
+from theater.models import Actor, Genre, Play, Performance
+from theater.serializers import ActorSerializer, GenreSerializer, PlayDetailSerializer, PlaySerializer, \
+    PerformanceDetailSerializer, PerformanceSerializer
+
 
 # Create your views here.
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class PlayViewSet(ModelViewSet):
+    queryset = Play.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+        return PlaySerializer
+
+
+class PerformanceViewSet(ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = GenreSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PerformanceDetailSerializer
+        return PerformanceSerializer
