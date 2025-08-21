8bc5158ee818d2e86b690883b190d9b028e0eb36
RomanSapunGit <sapunroma23@gmail.com>
2025-08-21 20:24:04 +0200

    feat(serializers): add and update serializers
    Added PlayList, Ticket, TicketSeats, PerformanceList, TicketList serializers for future use. Also updated (small and bigger changes) various serializers across the file.

--- theater/serializers.py
+++ theater/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from theater.models import Actor, Genre, Play, Performance, TheatreHall, Ticket, Reservation
@@ -19,6 +20,7 @@ class PlaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Play
         fields = (
+            "id",
             "title",
             "description",
             "actors",
@@ -36,11 +38,47 @@ class PlayDetailSerializer(PlaySerializer):
         many=True
     )
 
+    class Meta:
+        model = Play
+        fields = PlaySerializer.Meta.fields
+
+
+class PlayListSerializer(PlaySerializer):
+    genres = serializers.SlugRelatedField(
+        read_only=True,
+        many=True,
+        source="name"
+    )
+    actors = serializers.SlugRelatedField(
+        read_only=True,
+        many=True,
+        source="full_name"
+    )
+    tickets_available = serializers.IntegerField(
+        read_only=True
+    )
+
+    class Meta:
+        model = Play
+        fields = PlaySerializer.Meta.fields + ("tickets_available",)
+
 
 class TheatreHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = TheatreHall
         fields = ("id", "name", "rows", "seats_in_row")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "performance", "reservation")
+
+
+class TicketSeatsSerializer(TicketSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
 
 
 class PerformanceSerializer(serializers.ModelSerializer):
@@ -50,20 +88,62 @@ class PerformanceSerializer(serializers.ModelSerializer):
 
 
 class PerformanceDetailSerializer(PerformanceSerializer):
-    theater_hall = TheatreHallSerializer(many=True, read_only=True)
+    play = PlayDetailSerializer(read_only=True)
+    taken_places = TicketSeatsSerializer(
+        source="tickets",
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Performance
+        fields = ("id", "play", "theatre_hall", "show_time")
+
+
+class PerformanceListSerializer(PerformanceSerializer):
+    play = serializers.SlugRelatedField(
+        source="title"
+    )
+    theater_hall_name = serializers.CharField(
+        source="theatre_hall.name", read_only=True
+    )
+    theatre_hall_capacity = serializers.CharField(
+        source="theatre_hall.capacity", read_only=True
+    )
+
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "play",
+            "theater_hall_name",
+            "theatre_hall_capacity",
+            "show_time",
+        )
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
     class Meta:
         model = Reservation
         fields = ("id", "created_at", "user")
 
-
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = ("id", "row", "seat", "performance", "reservation")
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(reservation=reservation, **ticket_data)
+            return reservation
 
 
 class TicketDetailSerializer(TicketSerializer):
     performance = PerformanceDetailSerializer(read_only=True)
+
+
+class TicketListSerializer(TicketSerializer):
+    performance = PerformanceListSerializer(
+        many=False, read_only=True
+    )
