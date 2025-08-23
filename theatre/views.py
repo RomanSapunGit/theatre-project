683c1fe229b93712e49d5fa47e90fea85fb06d0b
RomanSapunGit <sapunroma23@gmail.com>
2025-08-23 20:34:29 +0200

    feat(actor views): add filter
    Added filtering by first and last name.

--- theatre/views.py
+++ theatre/views.py
@@ -16,6 +16,20 @@ class ActorViewSet(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
+    def get_queryset(self):
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        queryset = self.queryset
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset.distinct()
+
 
 class GenreViewSet(ModelViewSet):
     queryset = Genre.objects.all()
