2d3f9a0d2a7d1df491fbc98cd8fd7486492b590b
RomanSapunGit <sapunroma23@gmail.com>
2025-08-23 19:33:46 +0200

    feat(image): add create image file path

--- theatre/models.py
+++ theatre/models.py
@@ -1,6 +1,14 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
+
+
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/movies/", filename)
 
 
 class Actor(models.Model):
