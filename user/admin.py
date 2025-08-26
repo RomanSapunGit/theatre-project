6ff2ab26fecee638bedeb77191d4d4943890eb52
RomanSapunGit <sapunroma23@gmail.com>
2025-08-26 19:51:20 +0200

    refactor(user app): reformat code structure to follow best practices

--- user/admin.py
+++ user/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from user.models import User
 
+
 # Register your models here.
 @admin.register(User)
 class UserAdmin(UserAdmin):
