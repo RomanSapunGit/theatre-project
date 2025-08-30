547c44a7dd83dc55e315f7403c0313b22657435d
RomanSapunGit <sapunroma23@gmail.com>
2025-08-30 17:55:26 +0200

    refactor(play view set): update filtering
    Used django filter to replace boilerplate code.

--- theatre/views.py
+++ theatre/views.py
@@ -136,30 +136,6 @@ class PlayViewSet(
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @staticmethod
-    def _params_to_ints(qs):
-        return [int(str_id) for str_id in qs.split(",")]
-
-    def get_queryset(self):
-        title = self.request.query_params.get("title")
-        genres = self.request.query_params.get("genres")
-        actors = self.request.query_params.get("actors")
-
-        queryset = self.queryset
-
-        if title:
-            queryset = queryset.filter(title__icontains=title)
-
-        if genres:
-            genres_ids = self._params_to_ints(genres)
-            queryset = queryset.filter(genres__id__in=genres_ids)
-
-        if actors:
-            actors_ids = self._params_to_ints(actors)
-            queryset = queryset.filter(actors__id__in=actors_ids)
-
-        return queryset.distinct()
-
     @extend_schema(
         parameters=[
             OpenApiParameter(
