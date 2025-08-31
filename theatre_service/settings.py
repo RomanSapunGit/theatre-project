f3156098a162378fa77f29e07ef29f85b425f292
RomanSapunGit <sapunroma23@gmail.com>
2025-08-31 17:09:08 +0200

    feat(settings): populate allowed hosts

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ("django-insecure-"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
