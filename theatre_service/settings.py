9f9eaafcd576d9194513098f4bc4126c72046047
RomanSapunGit <sapunroma23@gmail.com>
2025-08-31 17:10:53 +0200

    feat(settings): hide secret key to env

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-"
-              "5c1-yj47*)5x8^il85&m19i$82wvnb$!-"
-              "c*4bsojkn4vq-_^5$"
-              )
-
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-d!z7!hh)4ly^*g#n(vapvlqs51onqs!o-b)hqhs%j&v_!bpguy"
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
