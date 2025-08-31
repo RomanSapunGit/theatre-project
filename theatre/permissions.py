bb40b8c55652d81cb7dc77adafe96bb78d4e50bb
RomanSapunGit <sapunroma23@gmail.com>
2025-08-31 17:14:02 +0200

    feat(theatre permissions): add is email verified condition

--- theatre/permissions.py
+++ theatre/permissions.py
@@ -4,9 +4,9 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def _is_read_only_allowed(self, request):
         return (
-            request.method in SAFE_METHODS
-            and request.user
-            and request.user.is_authenticated
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
         )
 
     def _is_admin(self, request):
@@ -14,19 +14,22 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return (
-            self._is_read_only_allowed(request)
-            or self._is_admin(request)
+                request.user.is_email_verified and
+                (self._is_read_only_allowed(request)
+                 or self._is_admin(request))
         )
 
 
 class IsAuthorizedOrIfAuthenticatedReadOnly(IsAdminOrIfAuthenticatedReadOnly):
     def has_permission(self, request, view):
         return (
-            self._is_read_only_allowed(request)
-            or self._is_admin(request)
-            or (
-                request.user
-                and request.user.is_authenticated
-                and request.user.is_hall_overseer
-                )
+                request.user.is_email_verified and
+                (self._is_read_only_allowed(request)
+                 or self._is_admin(request)
+                 or (
+                         request.user
+                         and request.user.is_authenticated
+                         and request.user.is_hall_overseer
+                 )
+                 )
         )
