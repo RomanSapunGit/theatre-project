c13603731a1529b944f6ff11a84a185464cc9df0
RomanSapunGit <sapunroma23@gmail.com>
2025-09-07 15:31:09 +0200

    refactor(user urls): update path endpoints

--- user/urls.py
+++ user/urls.py
@@ -13,9 +13,9 @@ from user.views import (
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("tokens/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("tokens/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("tokens/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path("verify-email/", VerifyUserEmailView.as_view(), name="email_verify")
 ]
