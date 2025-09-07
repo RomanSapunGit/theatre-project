e025814201d7473d39a54b0dfc20a77255efea18
RomanSapunGit <sapunroma23@gmail.com>
2025-09-07 15:35:43 +0200

    refactor(user urls): update path endpoints

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -28,13 +28,13 @@ from drf_spectacular.views import (
 urlpatterns = ([
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
-    path("api/user/", include("user.urls", namespace="user")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/doc/swagger/",
+    path("api/users/", include("user.urls", namespace="user")),
+    path("api/schemas/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/swagger/",
          SpectacularSwaggerView.as_view(url_name="schema"),
          name="swagger-ui"
          ),
-    path("api/doc/redoc/",
+    path("api/docs/redoc/",
          SpectacularRedocView.as_view(url_name="schema"),
          name="redoc"
          ),
